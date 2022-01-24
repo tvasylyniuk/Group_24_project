d36dee64e39e44556710a6ddacf3a87001937a75
Regina Musovirova <musovirova@yahoo.com>
2022-01-24 16:52:29 -0500

    main method created.

--- src/Regina_Folder/TestClass.java
+++ src/Regina_Folder/TestClass.java
@@ -2,6 +2,6 @@ package Regina_Folder;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("first");
+        System.out.println("first.");
     }
 }
